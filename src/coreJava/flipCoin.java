b45cdb1dc2550c6e99fe79ae04bd9411ac4cbe24
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-06-24 13:29:30 +0530

    flip coin program

--- src/coreJava/flipCoin.java
+++ src/coreJava/flipCoin.java
@@ -1,5 +1,7 @@
 package coreJava;
 
+import java.util.Scanner;
+
 public class flipCoin {
 
 	/**
@@ -7,7 +9,31 @@ public class flipCoin {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of times to flip coin");
+		
+		int count = scanner.nextInt();
+		int hcount = 0;
+		int tcount = 0;
+		
+		
+		for(int i=0; i< count; i++){
+			double num = Math.random();
+			if (num > 0.5){
+			  System.out.println("num12 "+ num);
+			  hcount = hcount + 1;
+			}
+			else{
+			  System.out.println("num 34"+num);
+			  tcount = tcount + 1;
+			}
+		}
+		
+		double perhead = (double)(((hcount * 100)/ count) );
+		float  pertail = (float)((( tcount * 100)/count));
+		
+		System.out.println("head count "+hcount +"head count percentage "+perhead);
+		System.out.println("tail count "+tcount +"tail count percentage "+pertail);
 	}
 
 }
